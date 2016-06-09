0788e18c82db89c8325d5a2cbe94929fd4f32f64
abilabbas <abilabbas11@gmail.com>
2016-06-10 04:24:13 +0700

    memasukkkan bahan dan edit index.jade

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,25 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke'),
-  cube = require('bespoke-theme-cube'),
   nebula = require('bespoke-theme-nebula'),
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
   bullets = require('bespoke-bullets'),
   backdrop = require('bespoke-backdrop'),
   scale = require('bespoke-scale'),
+  state = require('bespoke-state'),
   progress = require('bespoke-progress'),
   forms = require('bespoke-forms');
 
 // Bespoke.js
 bespoke.from('article', [
-  //cube(),
   nebula(),
   keys(),
   touch(),
-  bullets('li, .bullet'),
+  //bullets('li, .bullet'),
+  bullets('ul:not(.no-bullets) li, ol:not(.no-bullets) li, .bullet'),
   backdrop(),
   scale(),
+  state(),
   progress(),
   forms()
 ]);
